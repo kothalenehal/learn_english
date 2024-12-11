a01170b11f34dc08b46ac32df158a44ad5e1fde3
kothalenehal <nehal.kothale@gmail.com>
2024-12-11 15:19:21 +0530

    minor chang

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'b7c90b0142ce021ab988ab816b559d74b7caa454',
+        'reference' => 'c00e28f215dbd901d0f3335d5e5c7c7f8eb303df',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -331,7 +331,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'b7c90b0142ce021ab988ab816b559d74b7caa454',
+            'reference' => 'c00e28f215dbd901d0f3335d5e5c7c7f8eb303df',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
